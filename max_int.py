9a761e2f9b879de5f6ce1122e486f873cb89d1d6
Kristofer <Kristofergauti@gmail.com>
2019-09-05 15:41:40 +0000

    second commit

--- max_int.py
+++ max_int.py
@@ -10,3 +10,28 @@ max_int = max(listi)
 
 
 print("The maximum is", max_int)    # Do not change this line
+
+#dæmi 2
+n = int(input("Enter the length of the sequence: ")) # Do not change this line
+listi = []
+
+n_sum = 0
+
+for x in range(1,n+1):
+    if x <= 3:
+        n_sum = x
+        listi.append(x)
+    else:
+        next_num = (listi[x-2] + listi[x-3] + listi[x-4])
+        listi.append(next_num)
+
+for x in listi:
+    print(x,end=" ")
+    
+        
+    
+            
+    
+    
+    
+
